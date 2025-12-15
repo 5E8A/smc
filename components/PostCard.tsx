29f52c63d90ecf146295883d9c9712490c4f4a7d
logyQT <stoltmann96@gmail.com>
2025-12-16 00:16:13 +0100

    Refactor PostCard component and update BlogPost interface to include author details

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Calendar, User } from "lucide-react";
+import { Calendar /*User*/ } from "lucide-react";
 import { BlogPost } from "../types";
 import { useLanguage } from "../context/LanguageContext";
 
