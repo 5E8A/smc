856883cefa3152b5ea8be24e37f24335f7c16a6b
logyQT <33129241+logyQT@users.noreply.github.com>
2026-08-28 12:56:30 +0200

    feat(cms): crash error boundary with copyable bug report

--- @cms/src/main.tsx
+++ @cms/src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { RunConsoleProvider } from "./lib/RunConsoleProvider";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RunConsoleProvider>
-      <App />
-    </RunConsoleProvider>
+    <ErrorBoundary>
+      <RunConsoleProvider>
+        <App />
+      </RunConsoleProvider>
+    </ErrorBoundary>
   </StrictMode>
 );
