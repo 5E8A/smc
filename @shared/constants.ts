5d41e9e4ce74aa8b3c3fbb58574f91ee95b9eb87
logyQT <33129241+logyQT@users.noreply.github.com>
2026-09-07 09:34:17 +0200

    feat(shared): add constants for api bases, site base path and modrinth id

---
+++ @shared/constants.ts
@@ -0,0 +1,8 @@
+export const DISCORD_API_BASE = "https://discord.com/api/v10";
+export const MODRINTH_API_BASE = "https://api.modrinth.com/v2";
+export const MODRINTH_CDN = "https://cdn.modrinth.com/";
+
+export const SITE_BASE_PATH = "/smc";
+export const ASSETS_BASE_PATH = `${SITE_BASE_PATH}/assets`;
+
+export const MODRINTH_PROJECT_ID = "dOLVvHgi";
